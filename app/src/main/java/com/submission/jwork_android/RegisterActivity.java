f3299b8f14d96d2b1f02bbd533af1c71561b6af4
philipusrenaldy <philipusrenaldy@gmail.com>
2021-05-26 10:38:30 +0700

    m9 - TP

---
+++ app/src/main/java/com/submission/jwork_android/RegisterActivity.java
@@ -0,0 +1,52 @@
+package com.submission.jwork_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EditText etName = findViewById(R.id.et_name);
+        EditText etEmail = findViewById(R.id.et_registerEmail);
+        EditText etPassword = findViewById(R.id.et_registerPassword);
+        Button btnLogin = findViewById(R.id.btn_register);
+
+        btnLogin.setOnClickListener(v -> {
+            String name = etName.getText().toString();
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+
+            Response.Listener<String> responseListener = response -> {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    if (jsonObject != null) {
+                        Toast.makeText(RegisterActivity.this, "Register Successful", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(RegisterActivity.this, "Register Failed", Toast.LENGTH_SHORT).show();
+                }
+            };
+
+            RegisterRequest registerRequest = new RegisterRequest(name, email, password, responseListener);
+            RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+            queue.add(registerRequest);
+        });
+    }
+}
